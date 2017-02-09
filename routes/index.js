f114043301e2fd5055ff9c1567c10bb537460d06
Shawn <shawnmclau@gmail.com>
2017-02-09 16:39:12 -0500

    fix routing

--- routes/index.js
+++ routes/index.js
@@ -4,15 +4,30 @@ let router = express.Router();
 /* GET home page. */
 router.get('/', (req, res, next) => {
   let currentDate = new Date();
-  res.render('index', {
-    title: 'Express',
+  res.render('content/index', {
+    title: 'Home',
     date: currentDate.toLocaleString()
   });
 });
 
 /* GET about page. */
 router.get('/about', (req, res, next) => {
-  res.render('index', { title: 'About' });
+  res.render('content/about', { title: 'About' });
+});
+
+/* GET services page. */
+router.get('/services', (req, res, next) => {
+  res.render('content/services', { title: 'Services' });
+});
+
+/* GET projects page. */
+router.get('/projects', (req, res, next) => {
+  res.render('content/projects', { title: 'Projects' });
+});
+
+/* GET contact page. */
+router.get('/contact', (req, res, next) => {
+  res.render('content/contact', { title: 'Contact' });
 });
 
 module.exports = router;
